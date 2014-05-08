f6d654376cb78423094f1d4b224cc0b709f26330
Tom Sundstrom <office@tomsun.ax>
2014-05-08 17:42:11 +0300

    Load all tile images when the page loads
    Mitigates issues the first time each tile is shown in-game

--- js/atlantis.js
+++ js/atlantis.js
@@ -1,4 +1,25 @@
 function atlantis_init() {
+  var imgs_preload = new Array(
+    'action.png',
+    'asteria.png',
+    'atlantis.png',
+    'globalt.png',
+    'hyperion.png',
+    'lelantos.png',
+    'metis.png',
+    'oceanis.png',
+    'science.png',
+    'skapa.png',
+    'starfish.png',
+    'tavling.png',
+    'vatten.png'
+  )
+
+  var container = document.getElementsByClassName("container")[0]
+
+  for (var i in imgs_preload) {
+    container.innerHTML+= '<img class="preload" src="./img/' + imgs_preload[i] + '" />'
+  }
 }
 
 atlantis_init()
