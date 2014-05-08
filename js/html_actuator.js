ae15044b1833d5f60cb8b8b39d78e5e23ebbcbb5
Tom Sundstrom <office@tomsun.ax>
2014-05-08 14:03:24 +0300

    Customize end-of-game messages

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -142,7 +142,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "Du kom ända fram! #visespåatlantis" : "Nästan... #sesvipåatlantis?";
 
   if (typeof ga !== "undefined") {
     ga("send", "event", "game", "end", type, this.score);
