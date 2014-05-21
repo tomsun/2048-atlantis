f2ccc4fe558da5ec06172bc7f06709c42ab79da7
Tom Sundstrom <office@tomsun.ax>
2014-05-21 13:57:59 +0300

    Define Atlantis places

--- js/atlantis.js
+++ js/atlantis.js
@@ -1,3 +1,18 @@
+// Make sure this matches the CSS definition
+var atlantis_img_base = "http://tomsun.github.io/2048-atlantis/img";
+var atlantis_places = {};
+atlantis_places[2] = {"title": "Tävlingsdalen", "img": atlantis_img_base + "/tavling.png"};
+atlantis_places[4] = {"title": "Metis", "img": atlantis_img_base + "/metis.png"};
+atlantis_places[8] = {"title": "Skapadalen", "img": atlantis_img_base + "/skapa.png"};
+atlantis_places[16] = {"title": "Asteria", "img": atlantis_img_base + "/asteria.png"};
+atlantis_places[32] = {"title": "Lelantos", "img": atlantis_img_base + "/lelantos.png"};
+atlantis_places[64] = {"title": "Oceanis", "img": atlantis_img_base + "/oceanis.png"};
+atlantis_places[128] = {"title": "Hyperion", "img": atlantis_img_base + "/hyperion.png"};
+atlantis_places[256] = {"title": "Globalt-dalen", "img": atlantis_img_base + "/globalt.png"};
+atlantis_places[512] = {"title": "Actiondalen", "img": atlantis_img_base + "/action.png"};
+atlantis_places[2024] = {"title": "Vattendalen", "img": atlantis_img_base + "/vatten.png"};
+atlantis_places[4096] = {"title": "Atlantis", "img": atlantis_img_base + "/atlantis.png"};
+
 function atlantis_init() {
   var imgs_preload = new Array(
     'action.png',
