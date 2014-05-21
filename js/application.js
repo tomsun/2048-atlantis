133fbacf7768ec58a1900b8a53cbe8f5e4cee80a
Tom Sundstrom <office@tomsun.ax>
2014-05-21 13:20:11 +0300

    Keep a global reference to the game instance
    Shortcut for end-of-game augmentations: sharing etc

--- js/application.js
+++ js/application.js
@@ -1,4 +1,5 @@
 // Wait till the browser is ready to render the game (avoids glitches)
+var game;
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+  game = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
 });
